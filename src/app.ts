24ce8fc92806b8830289e76838c2c70091ef9555
gomjellie <gomjellie@gmail.com>
2021-07-26 10:12:50 +0900

    chore: app.ts, index.html을 README에 있는 counter 예제로 변경함

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,42 @@
 import html from './jsx';
+const state = {
+  counter: 0,
+};
 
-const hello = 'hello';
-const world = 'world';
+const setCounter = (val: number) => {
+  state.counter = val;
+  render();
+};
 
-const myComponent = html`
-  <div
-    class="${hello}"
-    onClick=${() => {
-      alert(hello);
-    }}
-  >
-    <span>${hello} ${world}</span>
-    <span></span>
-  </div>
-`;
+const containerClassName = 'counter-wrapper';
 
-document.body.appendChild(myComponent);
+function render() {
+  const $app = document.querySelector('#app');
+  const myComponent = html`
+    <div class="${containerClassName}">
+      <button
+        class="btn"
+        onClick=${() => {
+          setCounter(state.counter - 1);
+        }}
+      >
+        -
+      </button>
+      <span>${state.counter}</span>
+      <button
+        class="btn"
+        onClick=${() => {
+          setCounter(state.counter + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  `;
+  if ($app) {
+    $app.innerHTML = '';
+    $app.appendChild(myComponent);
+  }
+}
+
+render();
