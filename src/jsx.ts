0616d659dca5ca41fde7fddf9af80063c028968d
gomjellie <gomjellie@gmail.com>
2021-07-27 11:41:24 +0900

    feat: child Node의 배열 지원

--- src/jsx.ts
+++ src/jsx.ts
@@ -4,13 +4,12 @@ const DIRTY_REGEX_G = /dirtyindex:(\d+):/g;
 const DIRTY_SEPERATOR_REGEX_G = /(dirtyindex:\d+:)/g;
 
 /**
- * DOM Element 를 리턴합니다.
- * Element는 Node를 상속받은 클래스고
- * HTMLElement HTMLDivElement등은 Element를 상속받아서 구현합니다.
+ * DOM HTMLElement 를 리턴합니다.
+ * HTMLElement Node를 상속받은 클래스입니다.
  *
- * @returns {Element}
+ * @returns {HTMLElement}
  */
-const html = (strings: TemplateStringsArray, ...args: any[]): Element => {
+const html = (strings: TemplateStringsArray, ...args: any[]): HTMLElement => {
   if (!strings[0] && args.length) {
     throw new Error('Failed To Parse');
   }
@@ -64,7 +63,11 @@ const html = (strings: TemplateStringsArray, ...args: any[]): Element => {
 
       const arg = args[Number(dirtyIndex)];
       if (arg instanceof Node) return arg;
-
+      if (arg instanceof Array) {
+        const df = document.createDocumentFragment();
+        arg.forEach($el => df.appendChild($el));
+        return df;
+      }
       return buildDocumentFragmentWith(arg);
     });
 
@@ -100,7 +103,7 @@ const html = (strings: TemplateStringsArray, ...args: any[]): Element => {
     }
   }
 
-  return template.firstElementChild ?? template;
+  return <HTMLElement>template.firstElementChild ?? template;
 };
 
 export default html;
